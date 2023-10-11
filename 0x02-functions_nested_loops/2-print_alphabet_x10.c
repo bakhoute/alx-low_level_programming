dbe623f110f1baff0edffd8ae0bb4dfedf5d485a
bakhoute <bakhouteabdelbari.01@gmail.com>
2023-10-11 16:23:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ */
+void print_alphabet_x10(void)
+{
+	char num, letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
