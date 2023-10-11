3862b16eb72d7523477cd208711b4593b2ccdac2
bakhoute <bakhouteabdelbari.01@gmail.com>
2023-10-11 15:37:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (succeess)
  */
 int main(void)
 {
